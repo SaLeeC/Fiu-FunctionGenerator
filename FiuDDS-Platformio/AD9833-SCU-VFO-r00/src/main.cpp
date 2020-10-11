cf204abf567240582dac323342177b78faa96dc7
SaLeeC <20824335+SaLeeC@users.noreply.github.com>
2020-10-11 16:32:19 +0200

    Gestione DAC principale definita e da verificare

--- FiuDDS-Platformio/AD9833-SCU-VFO-r00/src/main.cpp
+++ FiuDDS-Platformio/AD9833-SCU-VFO-r00/src/main.cpp
@@ -3,15 +3,22 @@
 #include <ESP32DMASPISlave.h>
 
 void sweepgenCreate(float FL, float FH, float TSweep, uint8_t SweepMode);
+void DACIncrementStep(uint16_t ii);
+void DACDecrementStep(uint16_t ii);
+    
 //#define freqSerieMaxLeng 2048
-#define freqSerieMaxLeng 64
+#define freqSerieMaxLeng 2048
 //uint32_t freqSerie[freqSerieMaxLeng];//Sequenza di frequenze per lo sweep
 float freqSerie[freqSerieMaxLeng];//Sequenza di frequenze per lo sweep
 uint16_t freqSerieNumActiveElement = 0;//Numero degli elementi caricati per lo sweep
 uint32_t freqSerieTStep = 0;//Tempo in uS fra uno step e il successivo
-#define DACTotalStep 256.0
-float DACSerieKStep;
-uint16_t DACSerie[int(DACTotalStep)];
+#define DACResolution 256
+float DACSerieStep;
+float DACCurrentProg;
+//il numero di elementi dellArray del DAC è uguale al numero di elementi dell'Array
+//delle frequenze.
+//Il numero di celle utilizzate rimarà uguale a quello delle frequenze
+uint16_t DACSerie[freqSerieMaxLeng];
 
 ESP32DMASPI::Slave rxSPI;
 
@@ -102,7 +109,9 @@ da coprire sempre tutta l-escursione dello sweep.
 */
 {
   uint32_t deltaF;
-  //Inizializza il numero di elementi caricati nell'array al massimo valore caricabile
+  //Inizializza il numero di elementi utilizzati dell'array delle frequenze pari alla dimensione
+  //dellArray.
+  //Lo stesso valore è utilizzato per gestire lArray del DAC principale
   freqSerieNumActiveElement = freqSerieMaxLeng; 
   //Calcola il deltaF
   deltaF = FH - FL;
@@ -114,7 +123,7 @@ da coprire sempre tutta l-escursione dello sweep.
     freqSerieNumActiveElement = deltaF;
   }
   //calcola il rapporto fra i passi F e i passi V (passi del DAC)
-  DACSerieKStep = freqSerieNumActiveElement/DACTotalStep;
+  DACSerieStep = freqSerieNumActiveElement/DACResolution;
   //Inizializza la prima casella della serie
   //e calcola lo step di avanzamento per la frequenza nei casi in
   //cui il processo sia lineare
@@ -122,12 +131,14 @@ da coprire sempre tutta l-escursione dello sweep.
   //Se la ssequenza e inversa fa partire la serie dal valire massimo
   if (SweepMode & B00000001)
   {
-    freqSerie[0] = FH;
+    freqSerie[0] = FH;//Inizia dal valore massimo
+    DACSerie[0] = DACResolution;//Inizia dal valore massimo
   }
   //altrimenti parte dal valire minimo
   else
   {
-    freqSerie[0] = FL;
+    freqSerie[0] = FL;//Inizia dal valore minimo
+    DACSerie[0] = 0;//Inizia dal valore minimo
   }
   //Calcola il passo angolare in frazioni di 2Pigreco per la sinusoide
   float passoAngolare = (2.0 * PI) / freqSerieNumActiveElement;
@@ -135,15 +146,20 @@ da coprire sempre tutta l-escursione dello sweep.
   if((SweepMode & B00000010) != 0)
   {
       deltaF += deltaF;//Raddopia il passo
+      DACSerieStep += DACSerieStep;//Raddopia il passo
   }
-  //Controlla se è richiesta la sinusoide nel qual caso setta deltaF a delta/2 per snellire il calcolo
+  //Controlla se è richiesta la sinusoide nel qual caso setta deltaF a delta/2 per
+  //snellire il calcolo
   if((SweepMode & B00000100) != 0)
   {
     deltaF = (FH - FL) / 2.0;
+    DACSerieStep = freqSerieNumActiveElement / 2;//?????????
   }
   //Inizializza i riferimenti per la curva logaritmica
   float stepLog = (log10(FH) - log10(FL)) / freqSerieNumActiveElement;//calcola il passo per coprire l'intero sweep
-  //Apre il loop di popolamento dell'Array
+  //Inizializza laccumulatore per il controllo del passo del DAC
+  DACCurrentProg = DACSerie[0];
+  //Apre il loop di popolamento degli Array (frequenza e DAC)
   for(int32_t ii = 1; ii < freqSerieNumActiveElement; ii++)//per tutte le celle dell'Array
   {
     //Sceglie il processo di popolamento dell'Array
@@ -151,20 +167,24 @@ da coprire sempre tutta l-escursione dello sweep.
     {
     case 0://Rampa ascendente-popola per somma con andamento lineare
       freqSerie[ii] = freqSerie[ii - 1] + deltaF;
+      DACIncrementStep(ii);
       break;
     case 1://Rampa discendente-popola per differenza
       freqSerie[ii] = freqSerie[ii - 1] - deltaF;
+      DACDecrementStep(ii);
       break;
     case 2://Triangolare (simmetrica)-popola con un doppio passo
       //Per la prima metà dei passi va in salita
       if (ii <= (freqSerieNumActiveElement/2))
       {
         freqSerie[ii] = freqSerie[ii-1] + deltaF;
+        DACIncrementStep(ii);
       }
       //Per la seconda metà dei passi va in discesa
       else
       {
         freqSerie[ii] = freqSerie[ii-1] - deltaF;
+        DACDecrementStep(ii);
       }
       break;
     case 4://Sinusoidale
@@ -172,14 +192,51 @@ da coprire sempre tutta l-escursione dello sweep.
       //Lo split di (freqSerieNumActiveElement/4) serve a ritardare di 90° la simulazione
       //della sinusoide così da partire dal valore minimo (FL)
       freqSerie[ii] = (FL + deltaF) + (deltaF * sin((float(ii) - (freqSerieNumActiveElement/4.0)) * passoAngolare));
+      DACIncrementStep(ii);
       break;
     case 16://Rampa ascendente-popola per somma con andamento logaritmico
       freqSerie[ii] = pow(10,(log10(freqSerie[ii-1]) + stepLog));
+      DACIncrementStep(ii);
       break;
     default:
       break;
     }
   }
+
+  //Apre il loop di popolamento dellarray per il DAC
+  //Questa srie va popolata sempre tutta. La sequenza deve seguire il senso di crescita della frequenza
+  //così da assegnare sullasse delle X una posizione fissa ad ogni frequenza per cui, se la sequenza di sweep
+  //prevede un andamento misto della frequenza (triangolare o sinusoide), la tensione generata dal DAC
+  //seguirà lo stesso andamento.
+  //Il fattore di cresita di questo valore è legato SOLO al tempo trascorso e non all valore della frequenza
+  //esattamente come sarebbe il movimento dell'asse X di un oscilloscopio, ne consegue che nell'andamento
+  //logaritmico le curve derivanti saranno rappresentate come in un diagramma logaritmico e non linearizzate
+
+}
+
+void DACIncrementStep(uint16_t ii)
+{
+  DACCurrentProg += DACSerieStep;//incrementa del passo di avanzamento del DAC
+  //Se laccumulatore di passi ha superato lunità (il DAC lavora per numeri interi)
+  if(int(DACCurrentProg) >= 1)
+  {
+    //Incrementa il contenuto della cella corrente dellArray del DAC della parte intera
+    //dell'accumulatore di passo
+    DACSerie[ii] = DACSerie[ii-1] + int(DACCurrentProg);
+    //Elimina la parte intera dallaccumulatore di passo lasciando la parte decimale
+    DACCurrentProg -= int(DACCurrentProg);
+  }
+  //Se l'accumulatore di passo non ha ancora superato l'unità
+  else
+  {
+    //Conferma il valore precedente nella cella corrente dell'Array del DAC
+    DACSerie[ii] = DACSerie[ii-1];
+  } 
+}
+
+void DACDecrementStep(uint16_t ii)
+{
+    
 }
 
 void setup()
